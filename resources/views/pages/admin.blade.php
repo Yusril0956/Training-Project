94ff90a08ce4b49f8c0b69454728f9d092b8a928
ehan <ehan4426@gmail.com>
2025-08-20 14:27:45 +0700

    perbaikan

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -142,7 +142,8 @@
                 <div class="modal-body">
                   <div class="mb-3">
                     <label class="form-label">Name</label>
-                    <input type="text" class="form-control" name="name" value="{{  }}">
+                  <input type="text" class="form-control" name="name" value="{{ old('name', $user->name ?? '') }}">
+
                   </div>
                   <div class="mb-3">
                     <label class="form-label">Email</label>
