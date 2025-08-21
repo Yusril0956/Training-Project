177a3e2c23b761a0fb5054eab50a7f5d06bde0ea
yusril <miftahulyusril3@gmail.com>
2025-08-21 13:15:41 +0700

    menambah column baru untuk user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,14 +7,20 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    public function definition(): array
+    public function definition()
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => bcrypt('password'), // password default
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'nik' => $this->faker->numerify('################'),
+            'city' => $this->faker->city(),
+            'role' => 'user', // atau random dari ['user','admin','staff','super_admin']
+            'status' => 1, // jika ada kolom status
         ];
     }
 }
