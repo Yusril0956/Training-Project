3853daae3a25a84be2fa3935c1dedb0c50d08b56
ehan <ehan4426@gmail.com>
2025-08-27 14:07:01 +0700

    mandatory repan

--- routes/web.php
+++ routes/web.php
@@ -62,13 +62,3 @@ Route::middleware('auth')->group(function () {
     })->name('general.knowledge');
 });
 
-// route general knowledge
-Route::get('/General-Knowledge', function (){
-    return view('pages.Training.training1'); // nama file training1.blade.php
-})->name('general.knowledge');
-
-// route mandatory
-Route::get('/Mandatory', function (){
-    return view('pages.Training.training2');
-})->name('mandatory');
-
