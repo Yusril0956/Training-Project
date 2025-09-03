d393d51c177c2800c02f18aea50d2bb19701a0e7
Reqi <167438321+Reqi2007@users.noreply.github.com>
2025-09-03 10:37:08 +0700

    ganti rute sistem training di home.blade.php jadi 404 karena tidak ada apa apa

--- routes/web.php
+++ routes/web.php
@@ -144,7 +144,7 @@ Route::middleware('auth')->group(function () {
         Route::post('/admin/settings/open-access', [DashboardController::class, 'openAllAccess'])->name('admin.open.access');
         Route::post('/admin/settings/delete-database', [DashboardController::class, 'deleteDatabase'])->name('admin.delete.database');
         Route::post('/admin/settings/reset-password/{id}', [DashboardController::class, 'resetUserPassword'])->name('admin.reset.password');
-    });
+    });                                                   
 });
 
 
