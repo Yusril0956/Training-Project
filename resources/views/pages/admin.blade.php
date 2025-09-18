9adb35a6ec6133e649818881773188012301ac5c
Reqi <167438321+Reqi2007@users.noreply.github.com>
2025-09-18 14:41:50 +0700

    menghapus fitur yang sudah tidak digunakan dan dijadikan komen

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -276,7 +276,7 @@
     </div>
 
     <!-- Daftar Tugas -->
-<div class="card mt-4">
+<!-- <div class="card mt-4">
     <div class="card-header">
         <h3 class="card-title">📘 Daftar Tugas</h3>
         <a href="{{ route('assignments.create', $training->id ?? 1) }}" class="btn btn-sm btn-primary float-end">
@@ -315,7 +315,7 @@
             </tbody>
         </table>
     </div>
-</div>
+</div> -->
 
 
     @include('components._modal')
