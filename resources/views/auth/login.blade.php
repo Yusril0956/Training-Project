6ad6c4f747a2cec5cac3ed77f5064035f12da801
Reqi <167438321+Reqi2007@users.noreply.github.com>
2025-09-19 13:08:25 +0700

    KTP Ohim

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -123,12 +123,8 @@
                                         d="M12 2a9.96 9.96 0 0 1 6.29 2.226a1 1 0 0 1 .04 1.52l-1.51 1.362a1 1 0 0 1 -1.265 .06a6 6 0 1 0 2.103 6.836l.001 -.004h-3.66a1 1 0 0 1 -.992 -.883l-.007 -.117v-2a1 1 0 0 1 1 -1h6.945a1 1 0 0 1 .994 .89c.04 .367 .061 .737 .061 1.11c0 5.523 -4.477 10 -10 10s-10 -4.477 -10 -10s4.477 -10 10 -10z" />
                                 </svg>
                                 Login dengan google
-                            </a></div>
-                        <!--<div class="col"><a href="#" class="btn w-100">
-                  Download SVG icon from http://tabler-icons.io/i/brand-twitter
-                  <svg xmlns="http://www.w3.org/2000/svg" class="icon text-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z" /></svg>
-                  Login with Twitter
-                </a></div> -->
+                            </a>
+                            </div>
                     </div>
                 </div>
             </div>
