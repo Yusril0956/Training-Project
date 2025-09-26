abc6fb0c3b83c5fa35aa9786239adf0927dffc99
Reqi <167438321+Reqi2007@users.noreply.github.com>
2025-09-26 13:23:20 +0700

    ubah nama Aerospace menjadi PT.Dirgantara

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,7 +36,7 @@
             <div class="text-center mb-4">
                 <a href="." class="navbar-brand navbar-brand-autodark">
                     <img src="{{ asset('LogoBaru.png') }}" width="110" height="32" alt="Tabler"
-                        class="navbar-brand-image"> Aerospace
+                        class="navbar-brand-image"> PT.Dirgantara
                 </a>
             </div>
 
