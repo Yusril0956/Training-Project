cb2c3e17cd62c79b6da142472a021d3a45404d2a
Reqi <167438321+Reqi2007@users.noreply.github.com>
2025-09-26 10:02:14 +0700

    Fix tombol home logo di training

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -6,7 +6,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-                <a href="#">
+                <a href="{{ route('index') }}">
                     <img src="{{ asset('images/LOGOrl2.png') }}" class="navbar-brand-image "
                         alt="logo">PT.Dirgantara
                 </a>
