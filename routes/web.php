bedd7c9252751037b57e9a6597ff94cbe47fefd2
Reqi <167438321+Reqi2007@users.noreply.github.com>
2025-10-23 14:20:11 +0700

    menghapus /production-statistics

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,6 @@ Route::middleware('auth')->group(function () {
     Route::view('/help', 'dashboard.help')->name('help');
     Route::view('/services', 'dashboard.services')->name('services');
     Route::view('/company-detail', 'dashboard.company-detail')->name('company.detail');
-    Route::view('/production-statistics', 'dashboard.production-statistics')->name('production.statistics');
     Route::view('/laporan-data', 'dashboard.laporan-data')->name('laporan.data');
     Route::view('/kontak-divisi', 'dashboard.kontak-divisi')->name('kontak.divisi');
 
