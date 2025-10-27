e1bffd6e25090dc8bae813df2a8f683fdfb39606
Yusril0956 <miftahulyusril3@gmail.com>
2025-10-27 14:34:43 +0700

    package-lock.json dan vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-});
->>>>>>> d0db4e63200e75ccbc4e58ab2af404ba1359a0d4
-=======
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -11,10 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-    },
 });
-=======
-});
->>>>>>> d0db4e63200e75ccbc4e58ab2af404ba1359a0d4
